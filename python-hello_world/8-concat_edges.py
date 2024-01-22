f53e353b34b37b6cc466257adf9644549c6c3bf7
mistWil <7448@holbertonstudents.com>
2024-01-22 14:17:28 +0100

    8-concat_edges.py

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str.split()
+str = str[5] + " " + str[6] + " " + str[13] + " " + str[0]
+print(str)
