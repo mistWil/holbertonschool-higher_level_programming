bd5ffc1e6e66af06fd41b7771c32f4858c5c4f1b
mistWil <7448@holbertonstudents.com>
2024-01-24 17:32:58 +0100

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    ar = 1
+    args = sys.argv[1:]
+    if len(sys.argv) == 1:
+        print("0 argument:")
+    elif len(sys.argv) == 2:
+        print(len(sys.argv) - 1, "arguments:")
+    else:
+        print(len(sys.argv) - 1, "arguments:")
+    for i in range(len(args)):
+        print("{}: {}".format(ar, args[i]))
+        ar += 1
