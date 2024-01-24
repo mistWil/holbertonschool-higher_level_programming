fc0eb51b9ed4b34b0cd02be03426f1511a06b102
mistWil <7448@holbertonstudents.com>
2024-01-24 17:45:36 +0100

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     ar = 1
     args = sys.argv[1:]
     if len(sys.argv) == 1:
-        print("0 argument.")
+        print("0 arguments.")
     elif len(sys.argv) == 2:
         print(len(sys.argv) - 1, "argument:")
     else:
