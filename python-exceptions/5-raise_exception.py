c2e6cea93ec4d9215825511b0109a4f70fcd7dc8
mistWil <7448@holbertonstudents.com>
2024-02-02 10:53:19 +0100

    5-raise_exception.py

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""
+Write a function that raises a type exception.
+
+Prototype: def raise_exception():
+You are not allowed to import any module
+"""
+
+
+def raise_exception():
+    raise TypeError
