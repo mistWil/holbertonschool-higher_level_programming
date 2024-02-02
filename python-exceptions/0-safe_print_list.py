ec50cfbc7c50c20f309e501f3ca1941df5388437
mistWil <7448@holbertonstudents.com>
2024-02-02 10:24:11 +0100

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -16,12 +16,13 @@ You are not allowed to use len()
 
 """
 
+
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for i in range(0, x):
-                print("{}".format(my_list[i]), end="")
-                count += 1
+            print("{}".format(my_list[i]), end="")
+            count += 1
     except Exception:
         pass
     print()
