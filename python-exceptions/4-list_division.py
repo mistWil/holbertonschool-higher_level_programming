5ee45dba6e9d30e05aedb4b69a83f7b3a10e8dc0
mistWil <7448@holbertonstudents.com>
2024-02-02 10:48:57 +0100

    4-list_division.py

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+"""Write a function that divides element by element 2 lists.
+
+Prototype: def list_division(my_list_1, my_list_2, list_length):
+my_list_1 and my_list_2 can contain any type (integer, string, etc.)
+list_length can be bigger than the length of both lists
+Returns a new list (length = list_length) with all divisions
+If 2 elements can’t be divided, the division result should be equal to 0
+If an element is not an integer or float:
+print: wrong type
+If the division can’t be done (/0):
+print: division by 0
+If my_list_1 or my_list_2 is too short
+print: out of range
+You have to use try: / except: / finally:
+You are not allowed to import any module
+"""
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(0, list_length):
+        result = 0
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(result)
+    return new_list
