109e598e7f0fd8ffd6660dcb90e27bfd96ca470f
mistWil <7448@holbertonstudents.com>
2024-02-05 12:50:13 +0100

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+"""
+Write a class Square that defines a square by: (based on 1-square.py)
+
+Private instance attribute: size
+Instantiation with optional size: def __init__(self, size=0):
+size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+if size is less than 0, raise a ValueError exception with the message size must be >= 0
+You are not allowed to import any module
+"""
+
+
+class Square:
+    """
+    class Square that defines a square
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a square with a side of the specified size.
+
+        Args:
+            size (int): The size of the square's side.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size
