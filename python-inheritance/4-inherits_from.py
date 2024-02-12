4ef939e0a6151b49cb000bf8eb007f9f305cd904
mistWil <7448@holbertonstudents.com>
2024-02-12 14:20:42 +0100

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,6 @@
 Write a function that returns True if the object
 is an instance of a class that inherited (directly or indirectly)
 from the specified class ; otherwise False.
-
 Prototype: def inherits_from(obj, a_class):
 You are not allowed to import any module
 """
