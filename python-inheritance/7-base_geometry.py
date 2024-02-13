3ccce8f176e0df56126e4987d4da6c8c81602018
mistWil <7448@holbertonstudents.com>
2024-02-13 11:41:00 +0100

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 
+
+"""
+Write a class BaseGeometry (based on 6-base_geometry.py).
+
+Public instance method: def area(self): that raises an Exception
+with the message area() is not implemented
+Public instance method: def integer_validator(self, name, value):
+that validates value:
+you can assume name is always a string
+if value is not an integer: raise a TypeError exception, with the message
+<name> must be an integer
+if value is less or equal to 0: raise a ValueError exception with the
+message <name> must be greater than 0
+"""
+
+
 class BaseGeometry:
     """
     BaseGeometry class.
