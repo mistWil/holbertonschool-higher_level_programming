22c4f52e6dd1f5ee7ee3d0532776d8c02b818472
mistWil <7448@holbertonstudents.com>
2024-02-13 12:21:39 +0100

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -10,14 +10,19 @@
 
 
 class BaseGeometry:
-    """class 'BaseGeometry'"""
-
+    """
+    class 'BaseGeometry'
+    """
     def area(self):
-        """Public instance method that raises an exception"""
+        """
+        Public instance method that raises an exception
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Public instance method that validates value"""
+        """
+        Public instance method that validates value
+        """
         if not type(value) is int:
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
@@ -25,8 +30,9 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
-    """New class Rectangle that inherits from parent class"""
-
+    """
+    New class Rectangle that inherits from parent class
+    """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
