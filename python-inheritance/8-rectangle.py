34d4c30295b2ecfc499e9dc84d31f8444cac69d5
mistWil <7448@holbertonstudents.com>
2024-02-13 12:26:38 +0100

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -34,7 +34,8 @@ class Rectangle(BaseGeometry):
     New class Rectangle that inherits from parent class
     """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().__init__()
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
