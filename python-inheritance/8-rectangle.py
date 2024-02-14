59865eacd887d14af55e968767a43d92031a51fe
mistWil <7448@holbertonstudents.com>
2024-02-14 13:37:16 +0100

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 
 
+"""
+Import the BaseGeometry class
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-"""
-* Write a class BaseGeometry
-* Public instance method: that raises an Exception
-* Public instance method that validates value
-* You are not allowed to import any module
-"""
-
-
-
-
 class Rectangle(BaseGeometry):
-    """
-    New class Rectangle that inherits from parent class
-    """
+    """New class Rectangle that inherits from parent class"""
+
     def __init__(self, width, height):
-        super().__init__()
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
