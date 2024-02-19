e3924e2aef73cc6982a844909879cf5e68b11382
mistWil <7448@holbertonstudents.com>
2024-02-19 17:46:01 +0100

    models/rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -109,3 +109,19 @@ class Rectangle(Base):
     def __str__(self):
         return f'[Rectangle] ({self.id}) {self.x}/{self.y} - '\
             f'{self.width}/{self.height}'
+
+    def update(self, *args):
+        if len(args) > 0:
+            self.id = args[0]
+
+        if len(args) > 1:
+            self.width = args[1]
+
+        if len(args) > 2:
+            self.height = args[2]
+
+        if len(args) > 3:
+            self.x = args[3]
+
+        if len(args) > 4:
+            self.y = args[4]
