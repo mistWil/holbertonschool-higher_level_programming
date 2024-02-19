40dfb3d82e2497c28c11889e36f81526e346bc57
mistWil <7448@holbertonstudents.com>
2024-02-19 16:46:34 +0100

    models/rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -103,3 +103,6 @@ class Rectangle(Base):
         """Public method that prints the rectangle in stdout"""
         for i in range(self.height):
             print('#' * self.width)
+
+    def __str__(self):
+        return f'[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}'
