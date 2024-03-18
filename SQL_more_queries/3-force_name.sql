3b4c68ed4a5c069c5a9f3bbec9f718406342eb50
mistWil <7448@holbertonstudents.com>
2024-03-18 14:28:21 +0100

    3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -2,6 +2,6 @@
 
 -- Créer la table force_name si elle n'existe pas déjà
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT,
     name VARCHAR(256) NOT NULL
 );
