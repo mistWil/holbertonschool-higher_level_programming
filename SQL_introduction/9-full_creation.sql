d91d16a57926da42054216e73e79485106664e17
mistWil <7448@holbertonstudents.com>
2024-03-18 13:39:10 +0100

    9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- Script : create_second_table_and_insert_rows.sql
+
+-- Créer la table second_table si elle n'existe pas déjà
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insérer des lignes dans la table second_table
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
