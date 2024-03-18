131aa0d49d4868783eb590549a1351b21d7d95fa
mistWil <7448@holbertonstudents.com>
2024-03-18 14:19:26 +0100

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,7 @@
+-- Ce script affiche les privilèges accordés aux utilisateurs user_0d_1 et user_0d_2 sur le serveur MySQL (localhost).
+
+-- Afficher les privilèges de l'utilisateur user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Afficher les privilèges de l'utilisateur user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
