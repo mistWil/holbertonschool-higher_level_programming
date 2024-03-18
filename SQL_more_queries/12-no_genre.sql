e593e6d6ea2bb987d187e33110148e7319713b71
mistWil <7448@holbertonstudents.com>
2024-03-18 15:36:52 +0100

    12-no_genre.sql

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Script: list_shows_without_genre.sql
+
+-- Select all shows without a linked genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
