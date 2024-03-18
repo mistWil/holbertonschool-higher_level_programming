43738fe4555d6ca5d0857312fd7ab0b3a4ddfaba
mistWil <7448@holbertonstudents.com>
2024-03-18 14:44:08 +0100

    7-cities.sql

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,16 @@
+-- Script : create_database_and_table.sql
+
+-- Créer la base de données hbtn_0d_usa si elle n'existe pas déjà
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Utiliser la base de données hbtn_0d_usa
+USE hbtn_0d_usa;
+
+-- Créer la table cities si elle n'existe pas déjà
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
