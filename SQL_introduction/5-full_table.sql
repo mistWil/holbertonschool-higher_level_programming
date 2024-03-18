4a732d7d71448ad015ba0df5ca19c295c0126e18
mistWil <7448@holbertonstudents.com>
2024-03-18 12:01:45 +0100

    5-full_table.sql

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,11 @@
+-- Script : 5-full_table.sql
+
+-- Afficher la description complète de la table first_table
+SELECT
+    TABLE_NAME AS 'Table',
+    CREATE_TABLE AS 'Create Table'
+FROM
+    information_schema.TABLES
+WHERE
+    TABLE_SCHEMA = 'hbtn_0c_0'
+    AND TABLE_NAME = 'first_table';
