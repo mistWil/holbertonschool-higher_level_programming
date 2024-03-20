c89f1e782dcc374b9ef961fe3768cc455c1b177c
mistWil <7448@holbertonstudents.com>
2024-03-20 14:29:37 +0100

    16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,5 @@
--- Script: 16-shows_by_genre.sql
-
--- Select all shows and their genres
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+SELECT s.title, IFNULL(g.name, 'NULL') AS name
+FROM tv_shows s
+LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
+ORDER BY s.title ASC, name ASC;
