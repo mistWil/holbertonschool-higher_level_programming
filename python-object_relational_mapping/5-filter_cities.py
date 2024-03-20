e53ab012ff9c7f770b463fc25ce7642b6a0ed776
mistWil <7448@holbertonstudents.com>
2024-03-20 15:04:47 +0100

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
+
+
+"""
+This script takes in the name of a state as an
+argument and lists all cities of that state.
+"""
+
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
+
+    """
+    This block is executed when the script is run directly, not imported.
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    """
+    Get the command line arguments
+    """
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+    """
+    Establish a database connection
+    """
+
     cursor = db.cursor()
-    cursor.execute("""
-        SELECT cities.name 
-        FROM cities 
-        JOIN states ON cities.state_id = states.id 
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """, (state_name,))
-    rows = cursor.fetchall()
-    cities = [row[0] for row in rows]
-    print(", ".join(cities))
-    cursor.close()
-    db.close()
+    """
+    Get the cursor
+    """
+
+    cursor.execute((state_name,))
+    """
+    Execute the SQL command
+    """
