84c8835294d2a4dbd9ceaefc2a19421c2cf81a4f
mistWil <7448@holbertonstudents.com>
2024-03-20 14:19:45 +0100

    16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -5,4 +5,4 @@ SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
