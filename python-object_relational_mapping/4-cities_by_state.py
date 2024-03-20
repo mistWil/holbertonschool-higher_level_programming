61bae31219222b655e426ae9dc44525e6916bb6c
mistWil <7448@holbertonstudents.com>
2024-03-20 15:01:12 +0100

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python3
+
+
+"""
+This script lists all cities from the database hbtn_0e_4_usa.
+"""
+
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
+
+    """
+    This block is executed when the script is run directly, not imported.
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    """
+    Get the command line arguments
+    """
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+    """
+    Establish a database connection
+    """
+
     cursor = db.cursor()
-    cursor.execute("""
-        SELECT cities.id, cities.name, states.name 
-        FROM cities 
-        JOIN states ON cities.state_id = states.id 
-        ORDER BY cities.id ASC
-    """)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
+    """
+    Get the cursor
+    """
+
+    cursor.execute()
+
+    """
+    Execute the SQL command
+    """
