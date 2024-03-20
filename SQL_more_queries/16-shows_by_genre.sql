cb7ee3b7b612ad5c94bd52857a949b41b52e3725
mistWil <7448@holbertonstudents.com>
2024-03-20 14:36:06 +0100

    16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,9 @@
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, name ASC;
+-- Select the title of the show and the name of the genre. 
+-- If the genre is not available, display 'NULL'.
+SELECT tv_shows.title, tv_genres.name
+    -- From the table 'tv_shows'
+FROM
+    tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
