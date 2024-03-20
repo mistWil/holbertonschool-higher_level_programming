91f75a7252aa18cb34ed5d06d507b7b09b1cb808
mistWil <7448@holbertonstudents.com>
2024-03-20 14:31:22 +0100

    16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,8 @@
-SELECT s.title, IFNULL(g.name, 'NULL') AS name
+-- Script: 16-shows_by_genre.sql
+
+-- Select all shows and their genres
+SELECT CONCAT(s.title, ' - ', IFNULL(g.name, 'NULL')) AS 'show - genre'
 FROM tv_shows s
 LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
 LEFT JOIN tv_genres g ON sg.genre_id = g.id
-ORDER BY s.title ASC, name ASC;
+ORDER BY s.title ASC, g.name ASC;
