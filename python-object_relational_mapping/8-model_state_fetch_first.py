850c4cf897bdb61ec738f7c9f4c0cdfa651d8462
mistWil <7448@holbertonstudents.com>
2024-03-21 11:13:24 +0100

    8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
 
-from model_state import State
+
+"""Write a script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 import sys
 
-# Assurez-vous de définir Base et State ici si ils ne sont pas définis dans model_state
+
+# Assurez-vous de définir Base et State ici si ils ne sont
+# pas définis dans model_state
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Récupérer le premier état
-    state = session.query(State).order_by(State.id).first()
+    state = session.query(State).first()
 
     if state is not None:
         print("{}: {}".format(state.id, state.name))
