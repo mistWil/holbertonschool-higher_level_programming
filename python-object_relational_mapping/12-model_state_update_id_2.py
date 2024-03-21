e4b642fda79bbc9fe5102504cd7f9cd8477d87b6
mistWil <7448@holbertonstudents.com>
2024-03-21 11:31:20 +0100

    12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -14,14 +14,20 @@ import sys
 
 
 if __name__ == "__main__":
+    """including the guard clause"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    """creating a SQLAlchemy engine that will connect to the MySQL database"""
 
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    """ Changing the name of a state according to its id
+    using filter and update and commiting the changes
+    to the database
+    """
     session.query(State).filter(
         State.id == 2).update({"name": "New Mexico"})
-
     session.commit()
     session.close()
