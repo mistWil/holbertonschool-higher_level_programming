ddf463881c7766fe1c39d6780cb650ee02821ae3
mistWil <7448@holbertonstudents.com>
2024-03-21 10:24:36 +0100

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,13 +11,17 @@ import sys
 
 
 if __name__ == "__main__":
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3])
+        user=username,
+        passwd=password,
+        db=database)
 
     """
     Establish a database connection
