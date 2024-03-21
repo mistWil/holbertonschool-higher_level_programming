fb5888a463c603ce6adbee3a591b41bb0b88f3c5
mistWil <7448@holbertonstudents.com>
2024-03-21 10:48:20 +0100

    6-model_state.py

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-
-
+"""Start link class to table in database 
 """
-Start link class to table in database
-"""
-
-
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
