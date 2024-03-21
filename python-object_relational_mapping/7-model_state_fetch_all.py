65301e5a4176d89bc8fc7333c972d45a65b2347c
mistWil <7448@holbertonstudents.com>
2024-03-21 10:57:55 +0100

    7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
